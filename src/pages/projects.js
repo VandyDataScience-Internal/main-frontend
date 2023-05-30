18c9ea67357f3bd61e7250d89f698323c18399ba
A. Shrey <aditya.shrey4@gmail.com>
2023-05-30 11:39:19 -0700

    Updated formatting for Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,3 +1,6 @@
+// Font Update
+import '@fontsource/open-sans/700.css'
+
 import React from 'react';
 import {
   ChakraProvider,
@@ -10,75 +13,94 @@ import {
   WrapItem,
   Code,
   Grid,
-  theme,
   Card, 
   CardHeader, 
   CardBody, 
   CardFooter,
   SimpleGrid,
-  Button
+  Button,
+  Center,
+  extendTheme,
+  Container,
+  theme,
+  Stack,
+  Image,
+  Img,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+// Import Images
+import centParkImg from './projectinfo/images/consprkcent.png';
+import girlScoutImg from './projectinfo/images/girlscouts.png';
+import landTrustImg from './projectinfo/images/landtrust.png';
 
 const curProject = [
-  { title: 'Project 1', link: '/project1' },
-  { title: 'Project 2', link: '/project2' },
-  { title: 'Project 3', link: '/project3' },
-  { title: 'Project 4', link: '/project1' },
-  { title: 'Project 5', link: '/project2' },
+  { title: 'Centennial Park Conservancy', link: '/project1' , image: centParkImg},
+  { title: 'Girl Scouts of Middle Tennessee', link: '/project2', image: girlScoutImg},
+  { title: 'Geo-spatial Analysis for the Land Trust of Tennessee', link: '/project3', image:landTrustImg },
+  { title: 'Nash0, Striving for Zero Waste', link: '/project1', image: landTrustImg },
+  { title: 'Ancient Artifacts', link: '/project2', image:landTrustImg },
+  { title: 'Email Assist', link: '/project2', image:landTrustImg },
 ];
 
 const pastProject = [
   { title: 'Project 1', link: '/project1' },
-  { title: 'Project 2', link: '/project2' },
-  { title: 'Project 3', link: '/project3' },
-  { title: 'Project 4', link: '/project1' },
+  { title: 'Project 2', link: '/project1' },
+  { title: 'Project 3', link: '/project1' },
 ];
 
 const Projects = () => {
   return (
     <ChakraProvider theme={theme}>
-        <Heading as='h1' size='3xl' noOfLines={1}>
-            <Text as='u'>Current Projects</Text>
+        <Heading m={10} as='h1' size='3xl'>
+            <Center>
+              <Text fontFamily="Open Sans">Current Projects</Text>
+            </Center>
         </Heading>
-        <SimpleGrid m={4} spacing={8} templateColumns='repeat(auto-fill, minmax(400px, 2fr))'>
-        {curProject.map((project, index) => (
-            <Card key={index}>
-              <CardHeader>
-                <Heading size='md'>{project.title}</Heading>
-              </CardHeader>
-              <CardBody>
-                <Text>Project description goes here.</Text>
-              </CardBody>
-              <CardFooter>
-                <Button as='a' href={project.link}>View here</Button>
-              </CardFooter>
-            </Card>
+        <SimpleGrid m={10} spacing={10} templateColumns='repeat(auto-fill, minmax(400px, 2fr))'>
+         {curProject.map((project, index) => (
+            <Link href={project.link} _hover={{ textDecoration: 'none' }}>
+              <Card key={index} height="100%" >
+                <CardHeader>
+                  <Heading size='md'>{project.title}</Heading>
+                </CardHeader>
+                <Center>
+                  <CardBody mt={-5}>
+                    <Center>
+                      <Img
+                        src={project.image}
+                        borderRadius='lg'
+                      />
+                    </Center>
+                  </CardBody>
+                </Center>
+              </Card>
+            </Link>
           ))}
         </SimpleGrid>
-
-
-        <Heading as='h1' size='3xl' noOfLines={1}>
-           <Text as='u'>Past Projects</Text>
+        <Heading m={10} as='h1' size='3xl'>
+            <Center>
+              <Text fontFamily="Open Sans">Past Projects</Text>
+            </Center>
         </Heading>
-        <SimpleGrid m={4} spacing={8} templateColumns='repeat(auto-fill, minmax(400px, 2fr))'>
-        {pastProject.map((project, index) => (
-            <Card key={index}>
-              <CardHeader>
-                <Heading size='md'>{project.title}</Heading>
-              </CardHeader>
-              <CardBody>
-                <Text>Project description goes here.</Text>
-              </CardBody>
-              <CardFooter>
-                <Button as='a' href={project.link}>View here</Button>
-              </CardFooter>
-            </Card>
+        <SimpleGrid m={10} spacing={10} templateColumns='repeat(auto-fill, minmax(400px, 2fr))'>
+         {pastProject.map((project, index) => (
+            <Link href={project.link} _hover={{ textDecoration: 'none' }}>
+              <Card key={index}>
+                <CardHeader>
+                  <Heading size='md'>{project.title}</Heading>
+                </CardHeader>
+                <CardBody mt={-5}>
+                <Img
+                  src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                  alt='Green double couch with wooden legs'
+                  borderRadius='lg'
+                />
+                </CardBody>
+              </Card>
+            </Link>
           ))}
         </SimpleGrid>
-      </ChakraProvider>
-     
+    </ChakraProvider>
   );
 };
 
---
+++ src/projectinfo/centennialpark.js
@@ -0,0 +1,100 @@
+// Font Update
+import '@fontsource/open-sans/700.css'
+
+import React from 'react';
+import {
+  ChakraProvider,
+  Heading,
+  Box,
+  Text,
+  VStack,
+  theme,
+  Card, 
+  CardHeader, 
+  CardBody, 
+  CardFooter,
+  Center, 
+  Image,
+  Fade,
+  Circle,
+  SimpleGrid,
+  Container,
+  Stack,
+  StackDivider,
+} from '@chakra-ui/react';
+
+
+const ProjectTemplate = () => {
+  return (
+    <ChakraProvider theme={theme}>
+        <Center>
+          <Heading p={10} as='h1' size='3xl' >
+                <Text fontFamily="Open Sans"> Centennial Park Conservancy</Text>
+            </Heading>
+        </Center>
+
+          <Container maxW='container.lg'>
+            <Center>
+                <Text fontSize='2xl'>
+                  The Centennial Park Conservancy is a non-profit organization that collaborates with Metro Parks in order to preserve and enhance the Parthenon and Centennial Park. The Conservancy is dedicated to presenting accessible and inclusive programming, which they actively share with their community and visitors. However, the organization is currently facing challenges in comprehending and effectively utilizing the data gathered from their Google survey. This data plays a crucial role in informing their work.
+                  In order to improve the functionality of the Conservancy, a strategic decision was made by a dedicated team to develop a comprehensive dashboard. This dashboard aims to harness multiple data points obtained from the survey to address the key inquiries raised by the stakeholders. By visualizing the data in a structured manner, the Conservancy will be able to discern and comprehend the findings more accurately and effortlessly. This approach will facilitate a better understanding of the data, ultimately aiding the Conservancy in their mission.
+                </Text>
+            </Center>
+          </Container>
+
+        <Center>
+          <Heading pt={100} as='h1' size='2xl' >
+              <Text fontFamily="Open Sans"> Meet the Team</Text>
+          </Heading>
+        </Center>
+        <Center p={10}>
+          <Image
+                borderRadius='full'
+                boxSize='150px'
+                src='https://bit.ly/dan-abramov'
+                alt='Dan Abramov'
+            />
+        </Center>
+
+        <Center pb={10}>
+          <Card>
+            <CardBody>
+              <Stack divider={<StackDivider />} spacing='4'>
+                <Box>
+                  <Heading size='sm' textTransform='uppercase'>
+                    Engineering Manager
+                  </Heading>
+                  <Text pt='2' fontSize='md'>
+                    Adaline Adaline, 
+                    John
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size='sm' textTransform='uppercase'>
+                    Product Manager
+                  </Heading>
+                  <Text pt='2' fontSize='md'>
+                    Adaline Adaline
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size='sm' textTransform='uppercase'>
+                    Developer
+                  </Heading>
+                  <Text pt='2' fontSize='md'>
+                    Adaline Adaline
+                  </Text>
+                </Box>
+              </Stack>
+            </CardBody>
+          </Card>
+        </Center>
+
+          
+          
+      
+    </ChakraProvider>
+  );
+};
+
+export default ProjectTemplate;
