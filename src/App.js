66ba275a3e095ca6ba0a92a2373382c227e99f9a
Adaline <90662211+AdalineL@users.noreply.github.com>
2023-06-16 14:36:16 -0500

    cleaned up app.js and home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import InstaFeed from './Instagramposts';
 import Navbar from './nav/menuBar.js';
 import Home from './pages/home.js';
 import Projects from './pages/projects.js';
@@ -7,42 +6,20 @@ import Team from './pages/team.js';
 import Blog from './pages/blog.js';
 import Contact from './pages/contact.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ChakraProvider, Flex, VStack } from '@chakra-ui/react';
-import theme from './theme';
 
 function App() {
-  //console.log('test: ')
-  //console.log(feeds.data[0].media_url)
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/Projects" element={<Projects />} />
-          <Route path="/Team" element={<Team />} />
-          <Route path="/Blog" element={<Blog />} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
-
-      {/* <Router>
-        <Navbar />
-        <Routes></Routes>
-      </Router> */}
-      <ChakraProvider theme={theme}>
-        <Flex
-          minWidth="max-content"
-          alignItems="center"
-          textAlign="center"
-          fontSize="xl"
-        >
-          <VStack spacing={8}>
-            <Home />
-          </VStack>
-        </Flex>
-        <InstaFeed />
-      </ChakraProvider>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Team" element={<Team />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
 import { React, useRef, useState, useEffect } from 'react';
 import {
+  ChakraProvider,
   Box,
   Flex,
   HStack,
+  VStack,
   Link,
   IconButton,
   useDisclosure,
@@ -11,23 +13,26 @@ import {
   Heading,
   Text,
 } from '@chakra-ui/react';
-import backgroundImage from '../assets/background/datascience_background5.png'
-import backgroundImage2 from '../assets/background/datascience_background1.png'
-import logo from '../assets/background/logo.png'
+import InstaFeed from '../Instagramposts';
+import theme from '../theme';
+import backgroundImage from '../assets/background/datascience_background5.png';
+import backgroundImage2 from '../assets/background/datascience_background1.png';
+import logo from '../assets/background/logo.png';
 
 const Links = ['Dashboard', 'Projects', 'Team'];
 const NavLink = ({ children }) => (
-<Link
+  <Link
     px={2}
     py={1}
     rounded={'md'}
     _hover={{
-    textDecoration: 'underline', 
-    bg: useColorModeValue('gray.200', 'gray.700'),
+      textDecoration: 'underline',
+      bg: useColorModeValue('gray.200', 'gray.700'),
     }}
-    href={'#'}>
+    href={'#'}
+  >
     {children}
-</Link>
+  </Link>
 );
 
 export default function Home() {
@@ -53,56 +58,87 @@ export default function Home() {
     };
   }, []);
 
-
   return (
-    <>
-    <Box bg="white.100" px={4} borderBottom={"1px solid #f0ebf1"}>
-        <Flex h={16} alignItems="center" justifyContent="space-between" width={"96.22395vw"}>
-          <Image src={logo} alt="Logo" h="20vh" mr={8} mt={'2vh'}/> {/* adjust height and margin as needed */}
-          <HStack as="nav" spacing={4} display={{ base: "none", md: "flex" }}>
-            {Links.map((link) => (
-              <NavLink key={link}>{link}</NavLink>
-            ))}
-          </HStack>
-        </Flex>
-      </Box>
-    <div style={{ marginTop: "0", overflow:"hidden" }}>
-      <Image src={backgroundImage} 
-             objectFit="cover"
-             objectPosition={"center 20%"}
-             height={"70vh"}
-             width={"98.7239551vw"}
+    <ChakraProvider theme={theme}>
+      <Flex
+        minWidth="max-content"
+        alignItems="center"
+        textAlign="center"
+        fontSize="xl"
+      >
+        <VStack spacing={8}>
+          <Box bg="white.100" px={4} borderBottom={'1px solid #f0ebf1'}>
+            <Flex
+              h={16}
+              alignItems="center"
+              justifyContent="space-between"
+              width={'96.22395vw'}
+            >
+              <Image src={logo} alt="Logo" h="20vh" mr={8} mt={'2vh'} />{' '}
+              {/* adjust height and margin as needed */}
+              <HStack
+                as="nav"
+                spacing={4}
+                display={{ base: 'none', md: 'flex' }}
+              >
+                {Links.map(link => (
+                  <NavLink key={link}>{link}</NavLink>
+                ))}
+              </HStack>
+            </Flex>
+          </Box>
+          <div style={{ marginTop: '0', overflow: 'hidden' }}>
+            <Image
+              src={backgroundImage}
+              objectFit="cover"
+              objectPosition={'center 20%'}
+              height={'70vh'}
+              width={'98.7239551vw'}
             />
-    </div>
-    <div style={{ marginBottom: "35vh"}}>
-      <Heading size={"s"}>Welcome to the</Heading>
-      <Heading>Vanderbilt Data Science Club</Heading>
-    </div>
-    <Flex>
-        <Box width={"45vw"} display="flex" justifyContent="center">
-          <Image
-            ref={ref}
-            src={backgroundImage2}
-            fallbackSrc="https://via.placeholder.com/150"
-            opacity={isVisible ? 1 : 0}
-            transition="opacity 1s ease-in-out"
-            objectFit={"cover"}
-            height={"40vh"}
-          />
-        </Box>
-        <Box width={"45vw"} 
-             display="flex" 
-             justifyContent="center" 
-             flexDirection="column" 
-             alignItems="center"
-             ref={ref}
-             opacity={isVisible ? 1 : 0}
-             transition="opacity 1s ease-in-out">
-          <Heading size={'lg'}>Our Mission</Heading>
-          <Text>Vanderbilt Data Science aims to empower students from all experience levels to learn data science, artificial intelligence (AI), and machine learning (ML) through a project-based approach in which students will be able to have hands-on experience with real data while speaking with experienced industry professionals. Our goal is to become a community that encourages and actively promotes the learning of data science within and outside the classroom environment.</Text>
-        </Box>
+          </div>
+          <div style={{ marginBottom: '35vh' }}>
+            <Heading size={'s'}>Welcome to the</Heading>
+            <Heading>Vanderbilt Data Science Club</Heading>
+          </div>
+          <Flex>
+            <Box width={'45vw'} display="flex" justifyContent="center">
+              <Image
+                ref={ref}
+                src={backgroundImage2}
+                fallbackSrc="https://via.placeholder.com/150"
+                opacity={isVisible ? 1 : 0}
+                transition="opacity 1s ease-in-out"
+                objectFit={'cover'}
+                height={'40vh'}
+              />
+            </Box>
+            <Box
+              width={'45vw'}
+              display="flex"
+              justifyContent="center"
+              flexDirection="column"
+              alignItems="center"
+              ref={ref}
+              opacity={isVisible ? 1 : 0}
+              transition="opacity 1s ease-in-out"
+            >
+              <Heading size={'lg'}>Our Mission</Heading>
+              <Text>
+                Vanderbilt Data Science aims to empower students from all
+                experience levels to learn data science, artificial intelligence
+                (AI), and machine learning (ML) through a project-based approach
+                in which students will be able to have hands-on experience with
+                real data while speaking with experienced industry
+                professionals. Our goal is to become a community that encourages
+                and actively promotes the learning of data science within and
+                outside the classroom environment.
+              </Text>
+            </Box>
+          </Flex>
+          <Box height={'100vh'} bgColor={'white'}></Box>
+        </VStack>
       </Flex>
-      <Box height={"100vh"} bgColor={'white'}></Box>
-    </> 
+      <InstaFeed />
+    </ChakraProvider>
   );
-} 
+}
