38b2c07f3f3e3868e26dfba5a3012afca45681ef
Adaline <90662211+AdalineL@users.noreply.github.com>
2023-06-17 01:16:24 -0500

    fixed missing projectinfo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Navbar from './nav/menuBar.js';
 import Home from './pages/home.js';
 import Projects from './pages/projects.js';
 import Team from './pages/team.js';
 import Blog from './pages/blog.js';
 import Contact from './pages/contact.js';
+
+import CentParkPg from './assets/projectinfo/centennialpark';
+import EmailAssistPg from './assets/projectinfo/emailAssist';
+import GirlScoutsPg from './assets/projectinfo/girlscouts';
+import LandTrustPg from './assets/projectinfo/landtrust';
+import NashZeroPg from './assets/projectinfo/nash0';
+import AncientArtifactsPg from './assets/projectinfo/ancientArtifacts';
+import ProjectsMain from './assets/projectinfo/projectmain';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -18,6 +26,13 @@ function App() {
         <Route path="/Team" element={<Team />} />
         <Route path="/Blog" element={<Blog />} />
         <Route path="/Contact" element={<Contact />} />
+        <Route path="/" element={<ProjectsMain />} />
+        <Route path="/centennialpark" element={<CentParkPg />} />
+        <Route path="/emailAssist" element={<EmailAssistPg />} />
+        <Route path="/girlscouts" element={<GirlScoutsPg />} />
+        <Route path="/landtrusttn" element={<LandTrustPg />} />
+        <Route path="/nash0" element={<NashZeroPg />} />
+        <Route path="/ancientartifacts" element={<AncientArtifactsPg />} />
       </Routes>
     </BrowserRouter>
   );
