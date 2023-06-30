0fda3332e919cad45ae5c912a7f6dc4becb2afbd
djchakra <96687269+djchakra@users.noreply.github.com>
2023-06-29 23:52:40 -0500

    social buttons should work just need to replace the links

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -13,28 +13,40 @@ import { ReactNode } from 'react';
 
 export function SocialButton({ children, label, href }) {
   return (
-    <chakra.button href={href} marginLeft="25px" color="#FDDC7E">
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <chakra.button marginLeft="25px" color="#FDDC7E">
+        <VisuallyHidden>{label}</VisuallyHidden>
+        {children}
+      </chakra.button>
+    </a>
   );
 }
 
 const Footer = () => {
   return (
     <Box bg="grey" bottom="0" left="0" right="0">
+      {' '}
+      {/*I was able to flip the positions but i can't get the precise positioning right so far*/}
       <Container>
-        <a href="/">
-          <Image
-            filter="invert(1)"
-            src={process.env.PUBLIC_URL + '/vds-logo.PNG'}
-            alt="Logo"
-            boxSize="80px"
-            width="4.0wv"
-            paddingTop={15}
-          />
-        </a>
-        <Stack display="in-line" right="0" paddingLeft="80%">
+        <Stack right="0" paddingLeft="90%">
+          <a href="/">
+            {' '}
+            {/* The Vds logo*/}
+            <Image
+              filter="invert(1)"
+              src={process.env.PUBLIC_URL + '/vds-logo.PNG'}
+              alt="Logo"
+              boxSize="80px"
+              width="4.0wv"
+              paddingTop={10}
+            />
+          </a>
+        </Stack>
+
+        <Stack display="in-line" right="0" paddingBottom="0">
+          {' '}
+          {/*Row of social media buttons*/}
+          {/*Just sub the hashtags for what links you want to put and it should be good?*/}
           <SocialButton label={'Instagram'} href={'#'}>
             <SiInstagram />
           </SocialButton>
